0e9ce170afd9ec78d3a5572b023f9dfd26d247eb
hoangthai2171 <hoangthai2171@gmail.com>
2018-04-24 21:25:22 +0700

    Broadcast

---
+++ Broadcast/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+class Broadcast
+{
+    public static void Main()
+    {
+        Socket sock = new Socket(AddressFamily.InterNetwork, SocketType.Dgram, ProtocolType.Udp);
+        IPEndPoint iep1 = new IPEndPoint(IPAddress.Broadcast, 9050);
+        IPEndPoint iep2 = new IPEndPoint(IPAddress.Parse("172.16.101.12"), 9050);
+
+        //EndPoint ep = (IPEndPoint)iep1;
+        string hostname = Dns.GetHostName();
+        byte[] data = Encoding.ASCII.GetBytes(hostname);
+        sock.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.Broadcast, 1);
+        //sock.SendTo(data, iep1);
+        //sock.SendTo(data, iep2);
+
+
+        //sock.Bind(ep);
+        //sock.ReceiveFrom(data, ref ep);
+        //sock.SendTo(data, ep);
+        sock.Close();
+        Console.ReadLine();
+      
+
+    }
+     
+   
+    }
+
+
+
